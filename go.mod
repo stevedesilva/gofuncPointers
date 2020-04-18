f02f0b7bc8366ee71f576db443e99db7afe7ea13
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-04-18 08:11:07 +0100

    refactor

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/stevedesilva/gofuncPointers
 
 go 1.13
-
-require (
-	github.com/fatih/color v1.9.0 // indirect
-	github.com/inancgumus/prettyslice v0.0.0-20190305220808-d802ba58098f // indirect
-)
--- parser/parser.go
+++ parser/parser.go
@@ -6,61 +6,61 @@ import (
 	"strings"
 )
 
-// result stores the parsed result for a domain
-type result struct {
-	domain string
-	visits int
+// Result stores the parsed Result for a domain
+type Result struct {
+	Domain string
+	Visits int
 	// add more metrics if needed
 }
 
-// parser keep tracks of the parsing
-type parser struct {
-	sum     map[string]result // metrics per domain
-	domains []string          // unique domain names
-	total   int               // total visits for all domains
-	lines   int               // number of parsed lines (for the error messages)
+// Parser keep tracks of the parsing
+type Parser struct {
+	Sum     map[string]Result // metrics per domain
+	Domains []string          // unique domain names
+	Total   int               // total visits for all domains
+	Lines   int               // number of parsed lines (for the error messages)
 }
 
-// newParser constructs, initializes and returns a new parser
-func newParser() parser {
-	return parser{sum: make(map[string]result)}
+// NewParser constructs, initializes and returns a new Parser
+func NewParser() Parser {
+	return Parser{Sum: make(map[string]Result)}
 }
 
-// parse parses a log line and returns the parsed result with an error
-func parse(p parser, line string) (parsed result, err error) {
+// Parse parses a log line and returns the parsed Result with an error
+func Parse(p Parser, line string) (parsed Result, err error) {
 	fields := strings.Fields(line)
 	if len(fields) != 2 {
-		err = fmt.Errorf("wrong input: %v (line #%d)", fields, p.lines)
+		err = fmt.Errorf("wrong input: %v (line #%d)", fields, p.Lines)
 		return
 	}
 
-	parsed.domain = fields[0]
+	parsed.Domain = fields[0]
 
-	parsed.visits, err = strconv.Atoi(fields[1])
-	if parsed.visits < 0 || err != nil {
-		err = fmt.Errorf("wrong input: %q (line #%d)", fields[1], p.lines)
+	parsed.Visits, err = strconv.Atoi(fields[1])
+	if parsed.Visits < 0 || err != nil {
+		err = fmt.Errorf("wrong input: %q (line #%d)", fields[1], p.Lines)
 		return
 	}
 
 	return
 }
 
-// update updates the parser for the given parsing result
-func update(p parser, parsed result) parser {
-	domain, visits := parsed.domain, parsed.visits
+// Update updates the Parser for the given parsing Result
+func Update(p Parser, parsed Result) Parser {
+	domain, visits := parsed.Domain, parsed.Visits
 
 	// Collect the unique domains
-	if _, ok := p.sum[domain]; !ok {
-		p.domains = append(p.domains, domain)
+	if _, ok := p.Sum[domain]; !ok {
+		p.Domains = append(p.Domains, domain)
 	}
 
 	// Keep track of total and per domain visits
-	p.total += visits
+	p.Total += visits
 
 	// create and assign a new copy of `visit`
-	p.sum[domain] = result{
-		domain: domain,
-		visits: visits + p.sum[domain].visits,
+	p.Sum[domain] = Result{
+		Domain: domain,
+		Visits: visits + p.Sum[domain].Visits,
 	}
 
 	return p
